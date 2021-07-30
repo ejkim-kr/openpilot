5301cc05eac1d2826d8f17b069ef367781e99e5d
ejkim-kr <67148933+ejkim-kr@users.noreply.github.com>
2021-07-30 15:05:39 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -177,7 +177,7 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
+    candidate = "KIA NIRO EV 2020 PLATINUM"
 
   if CAR_FORCE_RECOGNITION is not None:
     candidate = CAR_FORCE_RECOGNITION
